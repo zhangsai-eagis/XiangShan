532d00bd42d464882d359ac6115d8cdb126706c3
Xuan Hu <huxuan@bosc.ac.cn>
2024-05-14 23:18:02 +0800

    NewCSR: make XRET use redirect bundle to pass target instead of csr-rob direct connection
    * TODO: remove isXRet signal from RobCSRIO.
    * Add ftqIdx,ftqOffset in CSR Fu.
    * XRet need not flushPipe

--- src/main/scala/xiangshan/backend/exu/ExeUnit.scala
+++ src/main/scala/xiangshan/backend/exu/ExeUnit.scala
@@ -246,7 +246,7 @@ class ExeUnitImp(
   // Assume that one fu can only write int or fp or vec,
   // otherwise, wenVec should be assigned to wen in fu.
   private val fuIntWenVec = funcUnits.map(x => x.cfg.needIntWen.B && x.io.out.bits.ctrl.rfWen.getOrElse(false.B))
-  private val fuFpWenVec  = funcUnits.map(x => x.cfg.needFpWen.B  && x.io.out.bits.ctrl.fpWen.getOrElse(false.B))
+  private val fuFpWenVec  = funcUnits.map( x => x.cfg.needFpWen.B  && x.io.out.bits.ctrl.fpWen.getOrElse(false.B))
   private val fuVecWenVec = funcUnits.map(x => x.cfg.needVecWen.B && x.io.out.bits.ctrl.vecWen.getOrElse(false.B))
   // FunctionUnits <---> ExeUnit.out
   io.out.valid := Cat(fuOutValidOH).orR
